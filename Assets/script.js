639e98024b8e622771efe5dd0cb207a20375214c
Saacmr <tristenwinton4@gmail.com>
2023-07-18 13:24:26 -0500

    cleaned up  js file

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 const localeSettings = {};
 dayjs.locale(localeSettings);
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   const currentHour = dayjs().format('H');
 
   function textEntry() {
@@ -20,12 +12,6 @@ $(function () {
     localStorage.setItem(key, value);
     });
   }
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   function hourlyColor() {
     $('.time-block').each(function() {
     const blockHour = parseInt(this.id);
@@ -46,19 +32,13 @@ $(function () {
     }
     });
   }
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
+  
   $('.time-block').each(function() {
     const key = $(this).attr('id');
     const value = localStorage.getItem(key);
     $(this).children('.description').val(value);
   });
 
-  // TODO: Add code to display the current date in the header of the page.
-  
   function updateTime() {
     const dateElement = $('#date');
     const timeElement = $('#time');
@@ -67,6 +47,7 @@ $(function () {
     dateElement.text(currentDate);
     timeElement.text(currentTime);
   }
+
   setInterval(updateTime, 1000);
   textEntry();
   hourlyColor();
